0f8ba3ae5a4495e19fb6ad994310239d4b374854
Patrik Ragan <ragan.patres@gmail.com>
2012-04-13 20:44:33 +0200

    added getters to Wheel

--- u/src/Components/Wheel.java
+++ u/src/Components/Wheel.java
@@ -4,47 +4,56 @@ import Enums.DiskBrands;
 import Enums.TireBrands;
 
 public class Wheel {
-	
+
 	private Disc disc;
 	private Tire tire;
-	
-	public void setDisc(Wheel.Disc disc){
+
+	public void setDisc(Wheel.Disc disc) {
 		this.disc = disc;
 	}
-	
-	public void setTire(Wheel.Tire tire){
+
+	public void setTire(Wheel.Tire tire) {
 		this.tire = tire;
 	}
-	
-	public class Disc{
-		
+
+	public Disc getDisc() {
+		return this.disc;
+	}
+
+	public Tire getTire() {
+		return this.tire;
+	}
+
+	public class Disc {
+
 		private int diameter;
-		//typ disku - true = elektron/false = plechovy
+		// typ disku - true = elektron/false = plechovy
 		private boolean isElectron;
-		//haveHubCup - puklica - moze mat, len ak to nie je elektron
+		// haveHubCup - puklica - moze mat, len ak to nie je elektron
 		private boolean haveHubCup;
 		private DiskBrands diskBrand;
-		
-		public Disc(int diameter, DiskBrands diskBrand, boolean isElectron, boolean haveHubCup){
+
+		public Disc(int diameter, DiskBrands diskBrand, boolean isElectron,
+				boolean haveHubCup) {
 			this.diameter = diameter;
 			this.isElectron = isElectron;
 			this.diskBrand = diskBrand;
 			this.haveHubCup = haveHubCup;
 		}
 	}
-	
-	public class Tire{
-		
+
+	public class Tire {
+
 		private int diameter;
 		private int tireWidth;
 		private TireBrands tireBrand;
-		
-		public Tire(int diameter, int tireWidth, TireBrands tireBrand){
+
+		public Tire(int diameter, int tireWidth, TireBrands tireBrand) {
 			this.diameter = diameter;
 			this.tireBrand = tireBrand;
 			this.tireWidth = tireWidth;
 		}
-		
+
 	}
 
 }
