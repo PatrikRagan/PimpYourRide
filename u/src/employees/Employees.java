68ef6bb3a400b9bf0601c053d3b6b60d003a7bea
Patrik Ragan <ragan.patres@gmail.com>
2012-04-17 16:04:16 +0200

    INTERIORER

--- u/src/employees/Employees.java
+++ u/src/employees/Employees.java
@@ -14,7 +14,7 @@ public class Employees {
 	}
 
 	public void addedEmployees() {
-		map.put(1, new Employee("Alojz", "Vesel7", Jobs.INERIORER));// engine,clutch
+		map.put(1, new Employee("Alojz", "Vesel7", Jobs.INTERIORER));// engine,clutch
 		map.put(2, new Employee("Adam", "Moravèík", Jobs.MECHANIC));// wheel,brakes,chasis,body kit
 		map.put(3, new Employee("František" , "Èerný", Jobs.MECHANIC));// features,interior,carRoof
 	}
--- u/src/enums/Jobs.java
+++ u/src/enums/Jobs.java
@@ -2,6 +2,6 @@ package enums;
 
 public enum Jobs {
 
-	MECHANIC, INERIORER
+	MECHANIC, INTERIORER
 	
 }
