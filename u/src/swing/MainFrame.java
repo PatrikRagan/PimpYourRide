5d071558fb9edea97fbd3410b9eb951b5e3b7b6e
Maros Kosina <maros.kosina@gmail.com>
2012-04-17 19:56:15 +0200

    LOG cez> public void addLog(String text)

--- u/src/swing/MainFrame.java
+++ u/src/swing/MainFrame.java
@@ -11,13 +11,14 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import components.*;
 import components.Interior.Seats;
 import employees.Employees;
 import enums.*;
-
 
 public class MainFrame extends JFrame {
 
@@ -30,7 +31,7 @@ public class MainFrame extends JFrame {
 	private String loggerText = "";
 	private DefaultMutableTreeNode node;
 	private JTree treeComponents;
-	
+
 	final JTextArea logArea = new JTextArea(20, 20);
 
 	public JTree getTreeComponents() {
@@ -56,122 +57,139 @@ public class MainFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 400);
 		setResizable(false);
-		setLocation(10,10);
+		setLocation(10, 10);
 		getContentPane().setLayout(new BorderLayout(0, 0));
-		
+
 		Employees.makeEmployees();
-		
+
 		JPanel bcgPanel = new JPanel();
 		bcgPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		getContentPane().add(bcgPanel, BorderLayout.CENTER);
 		bcgPanel.setLayout(null);
-		
+
 		JPanel leftPanel = new JPanel();
 		leftPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		leftPanel.setBounds(10, 11, 210, 352);
 		bcgPanel.add(leftPanel);
 		leftPanel.setLayout(null);
-		
+
 		final JComboBox compType = new JComboBox();
-		
-		 
+
 		JScrollPane scrollPane = new JScrollPane(logArea);
-		scrollPane.setViewportBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		scrollPane.setViewportBorder(new EtchedBorder(EtchedBorder.LOWERED,
+				null, null));
 		scrollPane.setBounds(230, 276, 350, 87);
 		bcgPanel.add(scrollPane);
-//		bcgPanel.add(logArea);
-       
-//       scrollpane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener()
-//       {
-//       public void adjustmentValueChanged(AdjustmentEvent ae)
-//       {
-//       ae.getAdjustable().setValue(ae.getAdjustable().getMaximum());
-//       }
-//       });
-       
+		// bcgPanel.add(logArea);
+
+		// scrollpane.getVerticalScrollBar().addAdjustmentListener(new
+		// AdjustmentListener()
+		// {
+		// public void adjustmentValueChanged(AdjustmentEvent ae)
+		// {
+		// ae.getAdjustable().setValue(ae.getAdjustable().getMaximum());
+		// }
+		// });
+
 		compType.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
-				addLog(compType.getSelectedItem().toString()+" was selected" +"\n");
-				
+
+				addLog(compType.getSelectedItem().toString() + " was selected"
+						+ "\n");
+
 			}
 		});
-		compType.setModel(new DefaultComboBoxModel(new String[] {"Strecha", "Podvozok", "Motor"}));
+		compType.setModel(new DefaultComboBoxModel(new String[] { "Strecha",
+				"Podvozok", "Motor" }));
 		compType.setBounds(10, 11, 190, 20);
 		leftPanel.add(compType);
-		
-		
+
 		JComboBox component = new JComboBox();
 		component.setBounds(10, 42, 190, 20);
 		leftPanel.add(component);
-		
+
 		JTextPane textPane = new JTextPane();
 		textPane.setBounds(10, 73, 190, 143);
 		leftPanel.add(textPane);
-		
-		//Obskuzna funkcia tlacitka INSTALL
+
+		// Obskuzna funkcia tlacitka INSTALL
 		JButton btnInstal = new JButton("Instal");
 		btnInstal.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				instalComponent();
 			}
 		});
-		
-		
+
 		btnInstal.setBounds(10, 289, 80, 23);
 		leftPanel.add(btnInstal);
-		
+
 		nameField = new JTextField();
 		nameField.setBounds(48, 227, 152, 20);
 		leftPanel.add(nameField);
 		nameField.setColumns(10);
-		
+
 		JLabel lblName = new JLabel("Name");
 		lblName.setBounds(10, 230, 46, 14);
 		leftPanel.add(lblName);
-		
+
 		passField = new JPasswordField();
 		passField.setBounds(48, 258, 152, 20);
 		leftPanel.add(passField);
-		
+
+		passField.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+			}
+
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+					instalComponent();
+				}
+			}
+		});
+
 		JLabel lblPass = new JLabel("Pass");
 		lblPass.setBounds(10, 261, 46, 14);
 		leftPanel.add(lblPass);
-		
-		
-		
 
-		
 		JPanel rightPanel = new JPanel();
-		rightPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		rightPanel
+				.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		rightPanel.setBounds(590, 11, 195, 352);
 		bcgPanel.add(rightPanel);
 		rightPanel.setLayout(null);
-		
+
 		JLabel lblCarComponents = new JLabel("CAR Components");
 		lblCarComponents.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblCarComponents.setBounds(10, 11, 124, 14);
 		rightPanel.add(lblCarComponents);
-		
+
 		JScrollPane scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(0, 30, 194, 322);
 		rightPanel.add(scrollPane_1);
-		
+
 		this.node = new DefaultMutableTreeNode("Components");
-		
-		//Engine
+
+		// Engine
 		DefaultMutableTreeNode engine = new DefaultMutableTreeNode("Engine");
 		node.add(engine);
 		DefaultMutableTreeNode cubature = new DefaultMutableTreeNode("cubature");
-		DefaultMutableTreeNode cylinders = new DefaultMutableTreeNode("cylinders");
+		DefaultMutableTreeNode cylinders = new DefaultMutableTreeNode(
+				"cylinders");
 		DefaultMutableTreeNode turbo = new DefaultMutableTreeNode("turbo");
 		DefaultMutableTreeNode fuel = new DefaultMutableTreeNode("fuel");
 		engine.add(cubature);
 		engine.add(cylinders);
 		engine.add(turbo);
 		engine.add(fuel);
-		
-		//Wheels
+
+		// Wheels
 		DefaultMutableTreeNode wheel = new DefaultMutableTreeNode("Wheels");
 		node.add(wheel);
 		DefaultMutableTreeNode tire = new DefaultMutableTreeNode("tire");
@@ -180,89 +198,95 @@ public class MainFrame extends JFrame {
 		wheel.add(tire);
 		wheel.add(disc);
 		wheel.add(chasis);
-		
-		//Brakes
+
+		// Brakes
 		DefaultMutableTreeNode brakes = new DefaultMutableTreeNode("Brakes");
 		node.add(brakes);
-		DefaultMutableTreeNode fdiameter = new DefaultMutableTreeNode("f-diameter");
-		DefaultMutableTreeNode fisDisc = new DefaultMutableTreeNode("f-brake type");
-		DefaultMutableTreeNode rdiameter = new DefaultMutableTreeNode("r-diameter");
-		DefaultMutableTreeNode risDisc = new DefaultMutableTreeNode("r-brake type");
+		DefaultMutableTreeNode fdiameter = new DefaultMutableTreeNode(
+				"f-diameter");
+		DefaultMutableTreeNode fisDisc = new DefaultMutableTreeNode(
+				"f-brake type");
+		DefaultMutableTreeNode rdiameter = new DefaultMutableTreeNode(
+				"r-diameter");
+		DefaultMutableTreeNode risDisc = new DefaultMutableTreeNode(
+				"r-brake type");
 		brakes.add(fdiameter);
 		brakes.add(fisDisc);
 		brakes.add(rdiameter);
 		brakes.add(risDisc);
-		
-		//Clutch
+
+		// Clutch
 		DefaultMutableTreeNode clutch = new DefaultMutableTreeNode("Clutch");
 		node.add(clutch);
 		DefaultMutableTreeNode gears = new DefaultMutableTreeNode("gears");
 		DefaultMutableTreeNode clutchType = new DefaultMutableTreeNode("type");
 		clutch.add(gears);
 		clutch.add(clutchType);
-		
-		//BodyKit
+
+		// BodyKit
 		DefaultMutableTreeNode body = new DefaultMutableTreeNode("Bodyshell");
 		node.add(body);
-		DefaultMutableTreeNode downforce = new DefaultMutableTreeNode("downforce");
+		DefaultMutableTreeNode downforce = new DefaultMutableTreeNode(
+				"downforce");
 		DefaultMutableTreeNode carRoof = new DefaultMutableTreeNode("roof");
 		body.add(downforce);
 		body.add(carRoof);
-		
-		//Interior
+
+		// Interior
 		DefaultMutableTreeNode interior = new DefaultMutableTreeNode("Interior");
 		node.add(interior);
-		DefaultMutableTreeNode interiorColor = new DefaultMutableTreeNode("color");
-		DefaultMutableTreeNode interiorMat = new DefaultMutableTreeNode("material");
+		DefaultMutableTreeNode interiorColor = new DefaultMutableTreeNode(
+				"color");
+		DefaultMutableTreeNode interiorMat = new DefaultMutableTreeNode(
+				"material");
 		DefaultMutableTreeNode seats = new DefaultMutableTreeNode("seat type");
-		DefaultMutableTreeNode seatsMat = new DefaultMutableTreeNode("seat material");
-		DefaultMutableTreeNode seatsHeated = new DefaultMutableTreeNode("heated seat");
-		DefaultMutableTreeNode seatsMassage = new DefaultMutableTreeNode("massage seat");
+		DefaultMutableTreeNode seatsMat = new DefaultMutableTreeNode(
+				"seat material");
+		DefaultMutableTreeNode seatsHeated = new DefaultMutableTreeNode(
+				"heated seat");
+		DefaultMutableTreeNode seatsMassage = new DefaultMutableTreeNode(
+				"massage seat");
 		interior.add(interiorColor);
 		interior.add(interiorMat);
 		interior.add(seats);
 		interior.add(seatsMat);
 		interior.add(seatsHeated);
 		interior.add(seatsMassage);
-		
-		//Features
+
+		// Features
 		DefaultMutableTreeNode features = new DefaultMutableTreeNode("Features");
 		node.add(features);
-		
+
 		treeComponents = new JTree(node);
 		scrollPane_1.setViewportView(treeComponents);
-		
+
 		JPanel carPanel = new JPanel();
 		carPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		carPanel.setBounds(230, 11, 350, 254);
 		bcgPanel.add(carPanel);
 		carPanel.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("CAR Info");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblNewLabel.setBounds(10, 11, 94, 20);
 		carPanel.add(lblNewLabel);
-		
 
-//    	compType.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//				loggerText += compType.getSelectedItem().toString()+" was selected" +"\n" ;
-//				logArea.setText(loggerText);
-//				logArea.setText(loggerText);
-//				
-//			}
-//		});
+		// compType.addActionListener(new ActionListener() {
+		// public void actionPerformed(ActionEvent arg0) {
+		// loggerText += compType.getSelectedItem().toString()+" was selected"
+		// +"\n" ;
+		// logArea.setText(loggerText);
+		// logArea.setText(loggerText);
+		//
+		// }
+		// });
 
-		
 	}
-	
-	private void instalComponent(){
-		addLog("INSTALLED");
+
+	private void instalComponent() {
+		addLog("INSTALLED\n");
 	}
-	
-	
-	
-	
+
 	/**
 	 * Launch the application.
 	 */
@@ -272,27 +296,30 @@ public class MainFrame extends JFrame {
 				try {
 					MainFrame frame = new MainFrame();
 					frame.setVisible(true);
-					
-					//Create car
+
+					// Create car
 					Wheel wheels = new Wheel();
 					wheels.setTire(wheels.new Tire(18, 220, TireBrands.DUNLOP));
 					wheels.setDisc(wheels.new Disc(18, true, false));
-					Engine engine = new Engine(2000,4,true,Fuel.DIESEL);
+					Engine engine = new Engine(2000, 4, true, Fuel.DIESEL);
 					Brakes front = new Brakes(30, true);
 					Brakes rear = new Brakes(26, false);
-					Interior interior = new Interior(Color.LIGHT_GRAY, Materials.ALCANTARA);
-					Seats seats = interior.new Seats(TypeOfSeats.CLASSIC, Materials.GENUINELEATHER, false, false);
+					Interior interior = new Interior(Color.LIGHT_GRAY,
+							Materials.ALCANTARA);
+					Seats seats = interior.new Seats(TypeOfSeats.CLASSIC,
+							Materials.GENUINELEATHER, false, false);
 					interior.setSeats(seats);
 					Clutch clutch = new Clutch(5, false);
 					BodyKit body = new BodyKit(10, 30);
 					ArrayList<Features> features = new ArrayList<Features>();
 					features.add(Features.DVD);
 					features.add(Features.AUDIO);
-					
-					Car car = new Car(wheels, Chasis.NORMAL, engine, 50, front, rear,
-							CarRoof.HARDTOP, interior, clutch, body, Color.BLACK, features);
-					//End creating car
-					
+
+					Car car = new Car(wheels, Chasis.NORMAL, engine, 50, front,
+							rear, CarRoof.HARDTOP, interior, clutch, body,
+							Color.BLACK, features);
+					// End creating car
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -307,11 +334,10 @@ public class MainFrame extends JFrame {
 	public JPasswordField getPassField() {
 		return passField;
 	}
-	
-	public void addLog(String text){
+
+	public void addLog(String text) {
 		this.loggerText += text;
 		logArea.setText(loggerText);
 	}
 
-	
 }
