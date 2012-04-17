4d8404df7b2d99a9eec7707ba6e11390a6097237
Maros Kosina <maros.kosina@gmail.com>
2012-04-17 17:42:08 +0200

    SecureAspect_base

--- u/src/employees/Employee.java
+++ u/src/employees/Employee.java
@@ -4,14 +4,18 @@ import enums.Jobs;
 
 public class Employee {
 
-	private final String name, surname;
+	private final String name, surname, nick, pass;
 	private final Jobs job;
+	
 	
 	
 	public Employee (String name, String surname, Jobs job){
 		this.name = name;
 		this.surname = surname;
 		this.job = job;
+		this.nick = name.substring(0, 0)+surname.substring(0,0);
+		pass = "123";
+		System.out.println(nick+"\n"+pass);
 	}
 
 
