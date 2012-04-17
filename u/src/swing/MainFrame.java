13d38bcdac6945344684fc5a647ddea7e51ed977
Maros Kosina <maros.kosina@gmail.com>
2012-04-17 09:37:57 +0200

    trosku loguje, mimo aspektov zatial layout uz by mohol byt final, este ten stred dorobim.

--- u/src/swing/MainFrame.java
+++ u/src/swing/MainFrame.java
@@ -5,6 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.*;
 import java.awt.BorderLayout;
 import javax.swing.border.EtchedBorder;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
 
 
 public class MainFrame extends JFrame {
@@ -15,6 +20,7 @@ public class MainFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JTextField textField;
 	private JPasswordField passwordField;
+	private String loggerText = "";
 
 	/**
 	 * Create the frame.
@@ -36,21 +42,33 @@ public class MainFrame extends JFrame {
 		bcgPanel.add(leftPanel);
 		leftPanel.setLayout(null);
 		
+		JPanel loggerPanel = new JPanel();
+		loggerPanel.setBounds(230, 273, 350, 90);
+		bcgPanel.add(loggerPanel);
+		loggerPanel.setLayout(new BorderLayout(0, 0));
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(10, 11, 190, 20);
-		leftPanel.add(comboBox);
+		final JTextPane loggerField = new JTextPane();
+		loggerPanel.add(loggerField, BorderLayout.CENTER);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(10, 42, 190, 20);
-		leftPanel.add(comboBox_1);
+		final JComboBox compType = new JComboBox();
 		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setBounds(10, 73, 190, 20);
-		leftPanel.add(comboBox_2);
+		compType.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				loggerText += compType.getSelectedItem().toString()+" was selected" +"\n" ;
+				loggerField.setText(loggerText);
+			}
+		});
+		compType.setModel(new DefaultComboBoxModel(new String[] {"Strecha", "Podvozok", "Motor"}));
+		compType.setBounds(10, 11, 190, 20);
+		leftPanel.add(compType);
+		
+		
+		JComboBox component = new JComboBox();
+		component.setBounds(10, 42, 190, 20);
+		leftPanel.add(component);
 		
 		JTextPane textPane = new JTextPane();
-		textPane.setBounds(10, 104, 190, 112);
+		textPane.setBounds(10, 73, 190, 143);
 		leftPanel.add(textPane);
 		
 		JButton btnInstal = new JButton("Instal");
@@ -74,23 +92,31 @@ public class MainFrame extends JFrame {
 		lblPass.setBounds(10, 261, 46, 14);
 		leftPanel.add(lblPass);
 		
-		JPanel loggerPanel = new JPanel();
-		loggerPanel.setBounds(230, 273, 350, 90);
-		bcgPanel.add(loggerPanel);
-		loggerPanel.setLayout(new BorderLayout(0, 0));
 		
-		JTextPane loggerField = new JTextPane();
-		loggerPanel.add(loggerField, BorderLayout.CENTER);
+		
+
 		
 		JPanel rightPanel = new JPanel();
 		rightPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		rightPanel.setBounds(590, 11, 144, 352);
 		bcgPanel.add(rightPanel);
+		rightPanel.setLayout(null);
+		
+		JLabel lblCarComponents = new JLabel("CAR Components");
+		lblCarComponents.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblCarComponents.setBounds(10, 11, 124, 14);
+		rightPanel.add(lblCarComponents);
 		
 		JPanel carPanel = new JPanel();
 		carPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		carPanel.setBounds(230, 11, 350, 254);
 		bcgPanel.add(carPanel);
+		carPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("CAR Info");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setBounds(10, 11, 94, 20);
+		carPanel.add(lblNewLabel);
 		
 		
 	}
