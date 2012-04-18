ca1325d68fdf8408a90009da60ed4d6323314ee4
Patrik Ragan <ragan.patres@gmail.com>
2012-04-18 04:27:49 +0200

    added serialization class

--- u/src/aspects/PersistenceAspect.aj
+++ u/src/aspects/PersistenceAspect.aj
@@ -1,5 +1,8 @@
 package aspects;
 
 public aspect PersistenceAspect {
-
+ int timeAmount;
+ public int calledCount = 0; 
+ //TODO: pocitadlo volani a cas ktory uplinie medzi volaniami
+ //TODO: pred volanim metodz zmeny Car zavola serializeNow
 }
--- u/src/pimpYourRide/SerializeCar.java
+++ u/src/pimpYourRide/SerializeCar.java
@@ -1,5 +1,23 @@
 package pimpYourRide;
 
-public class SerializeCar {
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
+public class SerializeCar implements Serializable {
+	private String filename = "file";
+
+	public void serializeNow() {
+		if (filename != null) {
+			try {
+				ObjectOutputStream out = new ObjectOutputStream(
+						new FileOutputStream(filename + ".ser"));
+
+				out.writeObject(Main.getInstance());
+				out.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
